403e0de1f6acb6827e585551629cf559713484c6
Jack <zediious@aol.com>
2025-02-20 18:42:54 -0500

    papyrus funcs
    Script on prone MGEF calls to set prone state when entering/leaving
    
    CanPlayerGetUp called OnUpdate when prone to check if able to get up

--- src/Papyrus.cpp
+++ src/Papyrus.cpp
@@ -1,6 +1,7 @@
 #include "Papyrus.h"
 
 #include "Settings.h"
+#include "AdjustmentHandler.h"
 
 namespace Papyrus
 {
@@ -17,10 +18,33 @@ namespace Papyrus
 		return true;
 	}
 
+	void DynamicCollisionAdjustment_Prone::SetProneState(RE::StaticFunctionTag*, bool proneState, bool sneakState)
+	{	
+		logger::debug("Function call was sent! {}", proneState);
+		AdjustmentHandler* AdjustmentHandlerSingleton = AdjustmentHandler::GetSingleton();
+		AdjustmentHandlerSingleton->ActorSneakStateChanged(RE::PlayerCharacter::GetSingleton()->GetHandle(), sneakState);
+	}
+
+	bool DynamicCollisionAdjustment_Prone::CanPlayerGetUp(RE::StaticFunctionTag*)
+	{	
+		AdjustmentHandler* AdjustmentHandlerSingleton = AdjustmentHandler::GetSingleton();
+		return AdjustmentHandlerSingleton->CheckEnoughSpaceToStand(RE::PlayerCharacter::GetSingleton()->GetHandle());
+	}
+
+	bool DynamicCollisionAdjustment_Prone::Register(RE::BSScript::IVirtualMachine* a_vm)
+	{
+		a_vm->RegisterFunction("SetProneState", "DynamicCollisionAdjustment_Prone", SetProneState);
+		a_vm->RegisterFunction("CanPlayerGetUp", "DynamicCollisionAdjustment_Prone", CanPlayerGetUp);
+
+		logger::info("Registered DynamicCollisionAdjustment_Prone class");
+		return true;
+	}
+
 	void Register()
 	{
 		auto papyrus = SKSE::GetPapyrusInterface();
 		papyrus->Register(DynamicCollisionAdjustment_MCM::Register);
+		papyrus->Register(DynamicCollisionAdjustment_Prone::Register);
 		logger::info("Registered papyrus functions");
 	}
 }
